9ae8e19f9c731bd305487f14e23ed5a8a28244d0
nadun96 <nadunudaraka@gmail.com>
2023-01-02 05:57:03 +0530

    001 - initial commit

--- core/signals.py
+++ core/signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 from django.db import models
-import time
+
 User = get_user_model()
 
 # auto username signal before save
